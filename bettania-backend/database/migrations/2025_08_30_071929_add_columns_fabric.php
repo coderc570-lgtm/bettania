be1109782a2462201fc15d2f13776917920f3f3f
Mark John Lagria <markjohnlagria8@gmail.com>
2025-08-30 15:25:46 +0800

    added fabric design

---
+++ bettania-backend/database/migrations/2025_08_30_071929_add_columns_fabric.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('fabrics', function (Blueprint $table) {
+            //
+            $table->string('color')->nullable();
+            $table->string('color_code')->nullable();
+            $table->unsignedBigInteger('fabric_design_id')->nullable();
+            $table->foreign('fabric_design_id')->references('id')->on('fabric_designs')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('fabrics', function (Blueprint $table) {
+            //
+        });
+    }
+};
