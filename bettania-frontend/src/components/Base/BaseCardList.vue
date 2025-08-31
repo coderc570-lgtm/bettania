eae08dba0f0182b43921b29abc2141c3386b2383
sigma <sigma@gmail.com>
2025-08-31 15:30:41 +0800

    new custom suit page

---
+++ bettania-frontend/src/components/Base/BaseCardList.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="flex flex-row w-full space-x-2
+        rounded-md
+        hover:text-gray-500
+        hover:outline
+        hover:outline-1 
+        hover:outline-gray-500"
+        >
+        <div class="p-2">
+          <img
+            src="@/assets/images/model-2.webp"
+            alt="fabric picture"
+            class="size-40 object-cover rounded-lg h-32 w-32"
+          >
+        </div>
+        <div class="flex flex-col justify-center">
+          <slot name="title"></slot>
+        </div>
+    </div>      
+</template>
---
+++ bettania-frontend/src/components/Base/BaseChips.vue
@@ -0,0 +1,27 @@
+<template>
+    <!-- <div> -->
+        <button 
+            type="button"
+            class="bg-gray-100 px-4 py-2 rounded-full text-gray-700 hover:bg-gray-200 
+                    focus:outline-none focus:ring-1 focus:ring-gray-300 
+                    transition duration-300 ease-in-out 
+                    truncate max-w-full"
+            @click="$emit('click')"
+            >
+            {{ title }}
+        </button>
+    <!-- </div> -->
+</template>
+
+<script setup lang="ts">
+ const props = defineProps<{
+    // imageSrc: string,
+    title: string,
+    // selected?: boolean
+    // customImgClass?: string
+  }>()
+  
+  defineEmits<{
+    (e: 'click'): void
+  }>()
+</script>
